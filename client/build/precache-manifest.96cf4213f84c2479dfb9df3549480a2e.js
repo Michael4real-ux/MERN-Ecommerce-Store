787c837bf941a3228d831f6e3cdb3a26aa946c93
Decagon <decagon@Michaels-Babalola-MacBook-Air.local>
2021-09-20 22:39:14 +0100

    new commit

---
+++ client/build/precache-manifest.96cf4213f84c2479dfb9df3549480a2e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83bc10214d957b152a77f6eeeb288466",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48a67105471578ff2a55",
+    "url": "/static/css/main.1edf2ec6.chunk.css"
+  },
+  {
+    "revision": "38e6e295405c1c8d5df2",
+    "url": "/static/js/2.25f416f2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.25f416f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48a67105471578ff2a55",
+    "url": "/static/js/main.33659206.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "8bf1af37dd02198b256725e83a203795",
+    "url": "/static/media/cart.8bf1af37.svg"
+  },
+  {
+    "revision": "2c5552a10c3ab0a115c9484f908f982d",
+    "url": "/static/media/close.2c5552a1.svg"
+  },
+  {
+    "revision": "b3f49afdaa769bc2cb267d590e308ae0",
+    "url": "/static/media/menu.b3f49afd.svg"
+  }
+]);
